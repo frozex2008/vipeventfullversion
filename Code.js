c5169ec16540827bc3e0df0a435dc268d6f57b54
frozex2008 <phanthanhlongp34@gmail.com>
2024-06-03 16:16:07 +0700

    Update Code.html

--- Code.js
+++ Code.js
@@ -1,14 +1,23 @@
-const inviteLinks = [
-    'https://invite.duolingo.com/family-plan/2-23R8-P8FS-12YF-H82G',
-    'https://invite.duolingo.com/family-plan/2-37ZY-11TM-T5HB-T3V8',
-    'https://invite.duolingo.com/family-plan/2-F1DE-T22H-W1TT-W2PV',
-    'https://invite.duolingo.com/family-plan/2-A51E-M8ND-C7U1-Z5QZ',
-    'https://invite.duolingo.com/family-plan/2-W8Y4-28PH-U38T-X7V2',
-    'https://invite.duolingo.com/family-plan/2-A877-J8J8-P7AV-E43F',
-    'https://invite.duolingo.com/family-plan/2-E7YR-T7XL-U1TE-C4RU',
-    'https://invite.duolingo.com/family-plan/2-Q6FZ-44JY-K3FP-17X8',
-    'https://invite.duolingo.com/family-plan/2-M7ME-H17Q-Z4SU-F7SJ',
-    'https://invite.duolingo.com/family-plan/2-17SA-P75P-T261-F7AS'
-];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open Links</title>
+</head>
+<body>
+    <button id="openLinkButton">Open Link</button>
+    <script>
+        const inviteLinks = [
+            'https://invite.duolingo.com/family-plan/2-17SA-P75P-T261-F7AS1',
+            'https://invite.duolingo.com/family-plan/2-17SA-P75P-T261-F7AS2',
+            'https://invite.duolingo.com/family-plan/2-17SA-P75P-T261-F7AS3'
+        ];
 
-module.exports = inviteLinks;
+        document.getElementById('openLinkButton').addEventListener('click', function() {
+            const randomIndex = Math.floor(Math.random() * inviteLinks.length);
+            window.open(inviteLinks[randomIndex], '_blank');
+        });
+    </script>
+</body>
+</html>
